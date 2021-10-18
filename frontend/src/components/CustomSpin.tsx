8760b05e97cbd41ace008c53a217c449947f938e
Karla Dampilag <karla.dmplg@gmail.com>
2021-10-18 07:52:02 +0200

    Finally make landing page work

--- frontend/src/components/CustomSpin.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Spin } from 'antd';
-
-const CustomSpin = () => (
-    <div style={{ 'textAlign': 'center', 'marginTop': '35px' }}>
-        <Spin tip='Loading...' size='large' />
-    </div>
-);
-export default CustomSpin;

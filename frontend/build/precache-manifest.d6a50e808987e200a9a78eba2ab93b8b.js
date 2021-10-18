f9b2f6cb05ae86c9204522b9460513d3b5c987d5
Karla Dampilag <karla.dmplg@gmail.com>
2021-10-18 08:02:53 +0200

    Fix errors for build to succeed

---
+++ frontend/build/precache-manifest.d6a50e808987e200a9a78eba2ab93b8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6be3d276cf45512f956589f297d1b78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc0d247a6995f95fb417",
+    "url": "/static/js/2.2b3bc1a0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2b3bc1a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad2fa1e439a8bb2d4a1",
+    "url": "/static/js/main.581cf2bf.chunk.js"
+  },
+  {
+    "revision": "20e92c8dba9e12b11291",
+    "url": "/static/js/runtime-main.8eabac95.js"
+  }
+]);
